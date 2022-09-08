c53b98a5d7b1e8e25beffe27c5f414f4f2653421
David Gasquez <davidgasquez@gmail.com>
2022-09-08 17:59:22 +0000

    style: 🎨

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,26 @@ use serde_json::Value;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-   /// Entity to export
-   #[clap(short, long, value_parser)]
-   entity: String,
+    /// Entity to export
+    #[clap(short, long, value_parser)]
+    entity: String,
 
-   /// Epoch to start the export from
-   #[clap(short, long, value_parser, default_value_t = 2100000)]
-   from: u32,
+    /// Epoch to start the export from
+    #[clap(short, long, value_parser, default_value_t = 2100000)]
+    from: u32,
 
-   /// Epoch to end the export
-   #[clap(short, long, value_parser, default_value_t = 2200000)]
-   to: u32,
+    /// Epoch to end the export
+    #[clap(short, long, value_parser, default_value_t = 2200000)]
+    to: u32,
 }
 
 fn main() {
     let args = Args::parse();
 
-    println!("Exporting {} from {} to {}", args.entity, args.from, args.to);
+    println!(
+        "Exporting {} from {} to {}",
+        args.entity, args.from, args.to
+    );
 
     let data = r#"
         {
@@ -35,9 +38,7 @@ fn main() {
     let v: Value = serde_json::from_str(data).unwrap();
 
     let client = reqwest::blocking::Client::new();
-    let res = client.post("https://api.node.glif.io")
-        .json(&v)
-        .send();
+    let res = client.post("https://api.node.glif.io").json(&v).send();
 
     let json = res.unwrap().json::<Value>().unwrap();
     println!("The latest height is: {}", json["result"]["Height"]);
