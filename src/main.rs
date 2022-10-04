edc6bd0d809be0f5a99e669345a1367a94df34b2
David Gasquez <davidgasquez@gmail.com>
2022-10-04 17:54:44 +0200

    feat: 🎨

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,18 @@ use serde_json::Value;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// Entity to export
-    #[clap(short, long, value_parser)]
-    entity: String,
-
     /// Epoch to start the export from
-    #[clap(short, long, value_parser, default_value_t = 2100000)]
+    #[clap(short, long, value_parser, default_value_t = 0)]
     from: u32,
 
     /// Epoch to end the export
-    #[clap(short, long, value_parser, default_value_t = 2200000)]
+    #[clap(short, long, value_parser, default_value_t = 0)]
     to: u32,
 }
 
-fn main() {
-    let args = Args::parse();
-
-    println!(
-        "Exporting {} from {} to {}",
-        args.entity, args.from, args.to
-    );
-
+// always returns 42
+fn get_chain_head() -> Result<u32, Box<dyn std::error::Error>> {
+    let client = reqwest::blocking::Client::new();
     let data = r#"
         {
             "jsonrpc": "2.0",
@@ -37,9 +28,20 @@ fn main() {
     // Parse the string of data into serde_json::Value.
     let v: Value = serde_json::from_str(data).unwrap();
 
-    let client = reqwest::blocking::Client::new();
     let res = client.post("https://api.node.glif.io").json(&v).send();
 
     let json = res.unwrap().json::<Value>().unwrap();
-    println!("The latest height is: {}", json["result"]["Height"]);
+    Ok(json["result"]["Height"].as_u64().unwrap() as u32)
+}
+
+fn main() {
+    let args = Args::parse();
+
+    // If both from and to are 0, start from the current chain head
+    if args.from == 0 && args.to == 0 {
+        println!("Following the chain from the current head");
+        println!("The chain head is: {}", get_chain_head().unwrap());
+    } else {
+        println!("Exporting from {} to {}", args.from, args.to)
+    }
 }
