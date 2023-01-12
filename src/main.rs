52849609906a741a178913f02493f5a0b91ad8d3
David Gasquez <davidgasquez@gmail.com>
2023-01-12 12:01:03 +0000

    fix: 🚑️ fix errors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use datafusion::execution::context::SessionContext;
-use deltalake::operations::DeltaOps;
 use deltalake::DeltaTableBuilder;
 
 use std::sync::Arc;
@@ -10,10 +9,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let table = DeltaTableBuilder::from_uri("delta_table").load().await?;
 
-    // let table = deltalake::open_table("delta_table").await.unwrap();
-
-    // let m = table.get_metadata().unwrap();
-    // print!("Table metadata: {:#?}", m);
+    let m = table.get_metadata().unwrap();
+    print!("Table metadata: {:#?}", m);
 
     // Register the table with the context
     if let Err(e) = ctx.register_table("delta_table", Arc::new(table)) {
@@ -23,20 +20,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Run SQL query
     let df = ctx.sql("SELECT * FROM delta_table").await.unwrap();
     df.show().await.unwrap();
-
-    let batches = ctx
-        .sql("SELECT * FROM delta_table")
-        .await
-        .unwrap()
-        .collect()
-        .await
-        .unwrap();
-
-    println!("Batches: {:#?}", batches);
-
-    for batch in batches {
-        let _table = DeltaOps(table).write(vec![batch.clone()]).await?;
-    }
 
     Ok(())
 }
